8772e867bf1b669aef6e6fb7777efaedde293acc
Ybarvaliya <uvlbarvaliya@gmail.com>
2024-06-14 13:00:21 +0530

    chat

--- chat/resources/views/welcome.blade.php
+++ chat/resources/views/welcome.blade.php
@@ -3,11 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('fonts/adminfont.css') }}">
 
         <!-- Styles -->
         <style>
