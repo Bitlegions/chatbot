17690c011ee920e90debf11881a542cba58e944f
harshil048 <harshilbambhroliya048@gmail.com>
2024-06-14 11:52:33 +0530

    Refactor bootstrap and jquery imports for better maintainability and performance

--- chat/resources/views/app.blade.php
+++ chat/resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chatbot Admin</title>
     <!-- Add Bootstrap CSS for styling -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap.min.css') }}">
 </head>
 
 <body>
@@ -15,9 +15,9 @@
     </div>
 
     <!-- Add Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('jquery/jquery.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('bootstrap/bootstrap.min.js') }}">
+
     @yield('scripts')
 </body>
 
